8fa2cb33b0620805d31d4876769680da90ed1d7e
Robin <robinburri@tutanota.com>
2021-09-26 19:48:11 +0200

    first commit

---
+++ ft_strdup.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+char    *ft_strdup(const char *s)
+{
+    int i;
+    int len;
+    char *ret;
+    i = 0;
+    len = ft_strlen(s);
+    
+    ret = (char *)malloc((len + 1) * sizeof(char));
+    if (ret == NULL)
+        return (NULL);
+    ft_strcpy(ret, s);
+    return (ret);
+}
---
+++ libft.h
@@ -0,0 +1,9 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+#endif
